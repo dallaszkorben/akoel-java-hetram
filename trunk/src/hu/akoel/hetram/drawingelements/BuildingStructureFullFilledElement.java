945c41f6570ba0c2aefa56f149c59fbc4ec862e3
dallaszkorben@gmail.com <dallaszkorben@gmail.com@9e802841-2ee9-0cd6-4cab-245090263c41>
2014-02-04 23:08:08 +0000

    Meg van az egy soros/oszlopos mintakitoltes. Dolgozom meg  a reszleteken lesz: fillColor, fillFullPattern, fillRowPattern

--- trunk/src/hu/akoel/hetram/drawingelements/BuildingStructureFullFilledElement.java
+++ trunk/src/hu/akoel/hetram/drawingelements/BuildingStructureFullFilledElement.java
@@ -68,10 +68,10 @@ public class BuildingStructureFullFilledElement extends DrawnBlock{
 		int patternWidth;
 		int patternHeight;
 
-		TexturePaint normalTexturePaint;
-		TexturePaint selectedTexturePaint;
-		TexturePaint infocusTexturePaint;
-		TexturePaint inprocessTexturePaint;
+		TexturePaint normalTexturePaint = null;
+		TexturePaint selectedTexturePaint = null;
+		TexturePaint infocusTexturePaint = null;
+		TexturePaint inprocessTexturePaint = null;
 
 		//Szelesebb mint magas
 		if( getWidth() > getHeight() ){
@@ -87,127 +87,147 @@ public class BuildingStructureFullFilledElement extends DrawnBlock{
 			
 		}
 	
-System.err.println(patternWidth + " - " + patternHeight );		
-if( patternWidth > 0 && patternHeight > 0 ){		
-		//
-		//Normal
-		//
-		BufferedImage bi1 = new BufferedImage( patternWidth, patternHeight, BufferedImage.TYPE_INT_RGB); 
-		Graphics2D big1 = bi1.createGraphics();
-		big1.setColor( background );
-		big1.fillRect( 0, 0, patternWidth, patternHeight );
-		big1.setColor( color ); 
-		
-		//
-		// Selected
-		//
-		BufferedImage bi2 = new BufferedImage( patternWidth, patternHeight, BufferedImage.TYPE_INT_RGB); 
-		Graphics2D big2 = bi2.createGraphics();
-		big2.setColor( SELECTED_BACKGROUND );
-		big2.fillRect( 0, 0, patternWidth, patternHeight );
-		big2.setColor( SELECTED_COLOR ); 
-		
-		//
-		// Infocus
-		//
-		BufferedImage bi3 = new BufferedImage( patternWidth, patternHeight, BufferedImage.TYPE_INT_RGB); 
-		Graphics2D big3 = bi3.createGraphics();
-		big3.setColor( background );
-		big3.fillRect( 0, 0, patternWidth, patternHeight );
-		big3.setColor( INFOCUS_COLOR ); 
-		
-		//
-		// Inprocess
-		//
-		BufferedImage bi4 = new BufferedImage( patternWidth, patternHeight, BufferedImage.TYPE_INT_RGB); 
-		Graphics2D big4 = bi4.createGraphics();
-		big4.setColor( background );
-		big4.fillRect( 0, 0, patternWidth, patternHeight );
-		big4.setColor( color ); 
-		
-		
-		
-		
-		//Szelesebb mint magas
-		if( getWidth() > getHeight() ){
-			
-			double shift = mainPanel.getCanvas().getPixelXPositionByWorldBeforeTranslate( getY1() ) - (int)( mainPanel.getCanvas().getPixelXPositionByWorldBeforeTranslate( getY1() ) / patternHeight );
-			big1.translate( 0,  shift );
-			big4.translate( 0,  shift );
-			
-//			big1.translate( 0,  mainPanel.getCanvas().getPixelYLengthByWorld(getY1() - (int)(getY1() / getHeight() ) ) );
-//			big4.translate( 0,  mainPanel.getCanvas().getPixelYLengthByWorld(getY1() - (int)(getY1() / getHeight() ) ) );
-			
-//			patternHeight = mainPanel.getCanvas().getPixelYLengthByWorld(getHeight());
-//			patternWidth = patternHeight / 2;
-			
+		//Ha nincs kiterjedese valamelyik iranyba, akkor nem lehet BufferedImage objektumot szerezni
+		//De mindegy is, mert akkor nem rajzolunk kitoltest, hiszen csak szel van
+		if( patternWidth > 0 && patternHeight > 0 ){		
+
 			//Normal
-			big1.drawLine( 0, 0, patternWidth / 2, patternHeight );
-			big1.drawLine( patternWidth, 0, patternWidth / 2, patternHeight );	
-			
-			//Selected
-			big2.drawLine( 0, 0, patternWidth / 2, patternHeight );
-			big2.drawLine( patternWidth, 0, patternWidth / 2, patternHeight );	
+			BufferedImage bi1 = new BufferedImage( patternWidth, patternHeight, BufferedImage.TYPE_INT_RGB); 
+			Graphics2D big1 = bi1.createGraphics();
+			big1.setColor( background );
+			big1.fillRect( 0, 0, patternWidth, patternHeight );
+			big1.setColor( color ); 
 		
-			//Infocus
-			big3.drawLine( 0, 0, patternWidth / 2, patternHeight );
-			big3.drawLine( patternWidth, 0, patternWidth / 2, patternHeight );	
-			
-			//Inprocess
-			big4.drawLine( 0, 0, patternWidth / 2, patternHeight );
-			big4.drawLine( patternWidth, 0, patternWidth / 2, patternHeight );	
+			// Selected
+			BufferedImage bi2 = new BufferedImage( patternWidth, patternHeight, BufferedImage.TYPE_INT_RGB); 
+			Graphics2D big2 = bi2.createGraphics();
+			big2.setColor( SELECTED_BACKGROUND );
+			big2.fillRect( 0, 0, patternWidth, patternHeight );
+			big2.setColor( SELECTED_COLOR ); 
+		
+			// Infocus
+			BufferedImage bi3 = new BufferedImage( patternWidth, patternHeight, BufferedImage.TYPE_INT_RGB); 
+			Graphics2D big3 = bi3.createGraphics();
+			big3.setColor( background );
+			big3.fillRect( 0, 0, patternWidth, patternHeight );
+			big3.setColor( INFOCUS_COLOR ); 
+		
+			// Inprocess
+			BufferedImage bi4 = new BufferedImage( patternWidth, patternHeight, BufferedImage.TYPE_INT_RGB); 
+			Graphics2D big4 = bi4.createGraphics();
+			big4.setColor( background );
+			big4.fillRect( 0, 0, patternWidth, patternHeight );
+			big4.setColor( color ); 
+		
+			//Szelesebb mint magas
+			if( getWidth() > getHeight() ){
 
+				int pos = mainPanel.getCanvas().getPixelYPositionByWorldBeforeTranslate( getY1() );
+				int shift = pos - ( (int)(pos / patternHeight ) ) * patternHeight;
 			
-		//Magasabb mint szeles
-		}else{
+				if( pos < 0 ){
+				
+					big1.translate(0, patternHeight);
+					big2.translate(0, patternHeight);
+					big3.translate(0, patternHeight);
+					big4.translate(0, patternHeight);
+				}
 			
-//			patternWidth = mainPanel.getCanvas().getPixelXLengthByWorld(getWidth());
-//			patternHeight = patternWidth / 2;
+				//Normal
+				big1.setStroke(NORMAL_STROKE);
+				big1.drawLine( 0, shift, patternWidth / 2, patternHeight + shift );
+				big1.drawLine( patternWidth, shift, patternWidth / 2, patternHeight + shift );	
+				
+				big1.drawLine( 0, shift-patternHeight, patternWidth / 2, shift );
+				big1.drawLine( patternWidth, shift-patternHeight, patternWidth / 2, shift );
 			
-			//Normal
-			big1.drawLine( 0, 0, patternWidth, patternHeight / 2 );
-			big1.drawLine( 0, patternHeight, patternWidth, patternHeight / 2 );			
+				//Selected
+				big2.setStroke(SELECTED_STROKE);
+				big2.drawLine( 0, shift, patternWidth / 2, patternHeight + shift );
+				big2.drawLine( patternWidth, shift, patternWidth / 2, patternHeight + shift );	
 			
-			//Selected
-			big2.drawLine( 0, 0, patternWidth, patternHeight / 2 );
-			big2.drawLine( 0, patternHeight, patternWidth, patternHeight / 2 );			
+				big2.drawLine( 0, shift-patternHeight, patternWidth / 2, shift );
+				big2.drawLine( patternWidth, shift-patternHeight, patternWidth / 2, shift );
+		
+				//Infocus
+				big3.setStroke(INFOCUS_STROKE);
+				big3.drawLine( 0, shift, patternWidth / 2, patternHeight + shift );
+				big3.drawLine( patternWidth, shift, patternWidth / 2, patternHeight + shift );	
+			
+				big3.drawLine( 0, shift-patternHeight, patternWidth / 2, shift );
+				big3.drawLine( patternWidth, shift-patternHeight, patternWidth / 2, shift );	
+			
+				//Inprocess
+				big4.setStroke(INPROCESS_STROKE);
+				big4.drawLine( 0, shift, patternWidth / 2, patternHeight + shift );
+				big4.drawLine( patternWidth, shift, patternWidth / 2, patternHeight + shift );	
+			
+				big4.drawLine( 0, shift-patternHeight, patternWidth / 2, shift );
+				big4.drawLine( patternWidth, shift-patternHeight, patternWidth / 2, shift );
+			
+			//Magasabb mint szeles
+			}else{
+			
+				int pos = mainPanel.getCanvas().getPixelXPositionByWorldBeforeTranslate( getX1() );
+				int shift = pos - ( (int)(pos / patternWidth ) ) * patternWidth;
+			
+				if( pos < 0 ){
+				
+					big1.translate( patternWidth, 0 );
+					big2.translate( patternWidth, 0 );
+					big3.translate( patternWidth, 0 );
+					big4.translate( patternWidth, 0 );
+				}
 
-			//Infocus
-			big3.drawLine( 0, 0, patternWidth, patternHeight / 2 );
-			big3.drawLine( 0, patternHeight, patternWidth, patternHeight / 2 );					 
-			 
-			//Inprocess
-			big4.drawLine( 0, 0, patternWidth, patternHeight / 2 );
-			big4.drawLine( 0, patternHeight, patternWidth, patternHeight / 2 );
+				//Normal
+				big1.setStroke(NORMAL_STROKE);
+				big1.drawLine( shift, 0, patternWidth + shift, patternHeight / 2 );
+				big1.drawLine( shift, patternHeight, patternWidth + shift, patternHeight / 2 );
 
+				big1.drawLine( shift - patternWidth, 0, shift, patternHeight / 2 );
+				big1.drawLine( shift - patternWidth, patternHeight, shift, patternHeight / 2 );
 			
+				//Selected
+				big2.setStroke(SELECTED_STROKE);
+				big2.drawLine( shift, 0, patternWidth + shift, patternHeight / 2 );
+				big2.drawLine( shift, patternHeight, patternWidth + shift, patternHeight / 2 );
+
+				big2.drawLine( shift - patternWidth, 0, shift, patternHeight / 2 );
+				big2.drawLine( shift - patternWidth, patternHeight, shift, patternHeight / 2 );
+			
+				//Infocus
+				big3.setStroke(INFOCUS_STROKE);
+				big3.drawLine( shift, 0, patternWidth + shift, patternHeight / 2 );
+				big3.drawLine( shift, patternHeight, patternWidth + shift, patternHeight / 2 );
+
+				big3.drawLine( shift - patternWidth, 0, shift, patternHeight / 2 );
+				big3.drawLine( shift - patternWidth, patternHeight, shift, patternHeight / 2 );
+			
+				//Inprocess
+				big4.setStroke(INPROCESS_STROKE);
+				big4.drawLine( shift, 0, patternWidth + shift, patternHeight / 2 );
+				big4.drawLine( shift, patternHeight, patternWidth + shift, patternHeight / 2 );
+
+				big4.drawLine( shift - patternWidth, 0, shift, patternHeight / 2 );
+				big4.drawLine( shift - patternWidth, patternHeight, shift, patternHeight / 2 );
+		
+			}
+		
+			Rectangle r = new Rectangle( 0, 0, patternWidth, patternHeight );
+			normalTexturePaint = new TexturePaint( bi1,r );
+			selectedTexturePaint = new TexturePaint( bi2,r ); 
+			infocusTexturePaint = new TexturePaint( bi3,r );
+			inprocessTexturePaint = new TexturePaint( bi4,r );
 		}
 		
-		
-		Rectangle r = new Rectangle( 0, 0, patternWidth, patternHeight );
-		normalTexturePaint = new TexturePaint( bi1,r );
-		selectedTexturePaint = new TexturePaint( bi2,r ); 
-		infocusTexturePaint = new TexturePaint( bi3,r );
-		inprocessTexturePaint = new TexturePaint( bi4,r );
-
 		setNormal( color, NORMAL_STROKE, normalTexturePaint );
 		setSelected( SELECTED_COLOR, SELECTED_STROKE, selectedTexturePaint );
 		setInfocus(INFOCUS_COLOR, INFOCUS_STROKE, infocusTexturePaint );
 		setInprocess( color, INPROCESS_STROKE, inprocessTexturePaint );
 		
 		super.draw(g2);
-//		g2.setPaint( inprocessTexturePaint );
-//		g2.fillRectangle( getX1(), getY1(), getX2(), getY2());
-//		g2.setStroke( INPROCESS_STROKE );
-		
-		
-}	
-		
-		g2.setColor( color );
-		g2.drawRectangle(getX1(), getY1(), getX2(), getY2());
-		
-	}	
-		
+
+	}			
 	
 }
 
